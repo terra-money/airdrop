5bf4e4ca954d3911326f485a728f30858896fa95
Javier Su Weijie <javier.su.weijie@gmail.com>
2022-06-10 17:45:56 +0800

    feat: added functions to verify eth and terra messages

---
+++ backend/src/helpers/verifiers/terra.ts
@@ -0,0 +1,16 @@
+import { Verifier } from "./interface";
+import { SimplePublicKey, Key } from "@terra-money/terra.js";
+import crypto from "crypto";
+import secp256k1 from "secp256k1";
+import { encode, decode } from "bech32-buffer";
+
+export class TerraVerifier implements Verifier {
+  verify(address: string, message: string, signature: string): boolean {
+    const hasher = crypto.createHash("SHA256");
+    const hash = hasher.update(message).digest();
+    const signatureBuffer = Buffer.from(signature, "hex");
+    const k = secp256k1.ecdsaRecover(signatureBuffer, 1, hash);
+    const pubKey = new SimplePublicKey(Buffer.from(k).toString("base64"));
+    return address === pubKey.address();
+  }
+}
