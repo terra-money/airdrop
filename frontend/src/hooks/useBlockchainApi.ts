1fa3633b82cce5e048e63ba40548c249833a92d1
plubber <51789398+ericHgorski@users.noreply.github.com>
2022-06-23 11:49:46 -0400

    Update frontend/src/hooks/useBlockchainApi.ts
    Co-authored-by: emidev98 <49301655+emidev98@users.noreply.github.com>

--- frontend/src/hooks/useBlockchainApi.ts
+++ frontend/src/hooks/useBlockchainApi.ts
@@ -6,7 +6,7 @@ const useBlockchainApi = () => {
     const isNewValidAccount = async (address: string): Promise<boolean> => {
         try {
             const { prefix: decodedPrefix } = decode(address)
-            const { data } = await axios.get(`https://phoenix-lcd.terra.dev/auth/accounts/${address}`);
+            const { data } = await axios.get(`https://lcd.terra.dev/cosmos/auth/v1beta1/accounts/${address}`);
             return data.height === "0" && decodedPrefix === "terra";
         }
         catch(e) {
