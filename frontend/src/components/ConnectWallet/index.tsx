26fd78a87ca99d580beab2b7f4be13fe74f49a0b
javiersuweijie <javier.su.weijie@gmail.com>
2022-07-26 10:42:00 +0800

    fixed warnings and errors

--- frontend/src/components/ConnectWallet/index.tsx
+++ frontend/src/components/ConnectWallet/index.tsx
@@ -1,164 +1,172 @@
-import { Alert, Button, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import { useConnectedWallet } from '@terra-money/wallet-provider';
-import { useSnackbar } from 'notistack';
-import { useEffect, useState } from 'react';
-import useWallets from '../../hooks/useWallets';
-import { Chain } from '../../models/Chain';
-import { Wallet } from '../../models/Wallet';
-import './ConnectWallet.scss'
+import {
+  Alert,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { useConnectedWallet } from "@terra-money/wallet-provider";
+import { useSnackbar } from "notistack";
+import { useEffect, useState } from "react";
+import useWallets from "../../hooks/useWallets";
+import { Chain } from "../../models/Chain";
+import { Wallet } from "../../models/Wallet";
+import "./ConnectWallet.scss";
 
 export type ConnectWalletType = {
-    wallets: Array<Wallet>,
-    onWalletConnected: (wallet: Wallet, chain: Chain) => void
-}
+  wallets: Array<Wallet>;
+  onWalletConnected: (wallet: Wallet, chain: Chain) => void;
+};
 
 export const ConnectWallet = (props: ConnectWalletType) => {
-    const {
-        wallets,
-        onWalletConnected
-    } = props;
+  const { wallets, onWalletConnected } = props;
 
-    const [wallet, setWallet] = useState<Wallet | null>();
-    const [walletId, setWalletId] = useState('');
+  const [wallet, setWallet] = useState<Wallet | null>();
+  const [walletId, setWalletId] = useState("");
 
-    const [chains, setChains] = useState(new Array<Chain>());
-    const [chain, setChain] = useState<Chain | null>();
-    const [chainId, setChainId] = useState('');
+  const [chains, setChains] = useState(new Array<Chain>());
+  const [chain, setChain] = useState<Chain | null>();
+  const [chainId, setChainId] = useState("");
 
-    const [walletInstalled, setWalletInstalled] = useState<boolean>();
+  const [walletInstalled, setWalletInstalled] = useState<boolean>();
 
-    const { enqueueSnackbar } = useSnackbar();
-    const {
-        isInstalled,
-        isConnected,
-        connect
-    } = useWallets();
-    const connectedWallet = useConnectedWallet();
+  const { enqueueSnackbar } = useSnackbar();
+  const { isInstalled, isConnected, connect } = useWallets();
+  const connectedWallet = useConnectedWallet();
 
-    // Imperative way to detect if station is connected
-    useEffect(() => {
-        if (connectedWallet && wallet && chain) {
-            onWalletConnected(wallet, chain)
-        }
-    }, [connectedWallet])
-
-
-    const handleSelectWallet = (event: any) => {
-        const selectedWallet = wallets.find(wallet => wallet.id === event.target.value);
-        setChain(null);
-        setChainId("");
-        setChains([]);
-
-        if (selectedWallet) {
-            setWalletId(event.target.value);
-            setWallet(selectedWallet);
-            setChains(selectedWallet.chains);
-
-            const firstChain = selectedWallet.chains[0];
-            if(selectedWallet.chains.length === 1) {
-                setChain(firstChain);
-                setChainId(firstChain.id);
-            }
-
-            const walletInstalled = isInstalled(selectedWallet);
-            setWalletInstalled(walletInstalled);
-        }
-    };
-
-    const handleSelectChain = (event: any) => {
-        const selectedChain = chains.find(chain => chain.id === event.target.value);
-
-        if (wallet) {
-            setChainId(event.target.value);
-            setChain(selectedChain);
-
-            if (isConnected(wallet) && selectedChain) {
-                onWalletConnected(wallet, selectedChain)
-            }
-        }
-    };
-
-
-    const handleConnectWallet = async () => {
-        try {
-            if (wallet && chain) {
-                await connect(wallet, chain);
-
-                // For station and wallet connect 
-                // the status must be checked on 
-                // an imperative way thru useEffect
-                if (wallet.id !== "station" && wallet.id !== "stationmobile") {
-                    onWalletConnected(wallet, chain);
-                }
-            }
-        }
-        catch (e) {
-            console.log(e);
-            enqueueSnackbar("Operation cancelled", { variant: "error" });
-        }
+  // Imperative way to detect if station is connected
+  useEffect(() => {
+    if (connectedWallet && wallet && chain) {
+      onWalletConnected(wallet, chain);
     }
+  }, [connectedWallet, wallet, chain, onWalletConnected]);
 
-    const handleCleanSelections = () => {
-        setWallet(null);
-        setWalletId('');
-        setChain(null);
-        setChainId('');
-        setChains([]);
-        setWalletInstalled(false);
+  const handleSelectWallet = (event: any) => {
+    const selectedWallet = wallets.find(
+      (wallet) => wallet.id === event.target.value
+    );
+    setChain(null);
+    setChainId("");
+    setChains([]);
+
+    if (selectedWallet) {
+      setWalletId(event.target.value);
+      setWallet(selectedWallet);
+      setChains(selectedWallet.chains);
+
+      const firstChain = selectedWallet.chains[0];
+      if (selectedWallet.chains.length === 1) {
+        setChain(firstChain);
+        setChainId(firstChain.id);
+      }
+
+      const walletInstalled = isInstalled(selectedWallet);
+      setWalletInstalled(walletInstalled);
     }
+  };
 
-    return (
-        <div className='ConnectWallet'>
-            <h4>Select a wallet and connect to an available network. Then, check your wallet's airdrop eligibility, sign a transaction to prove ownership and claim your airdrop.</h4>
-            <FormControl className='FormControl' fullWidth>
-                <InputLabel id='WalletLabel'>Select Wallet</InputLabel>
-                <Select
-                    id='WalletDropdown'
-                    labelId='Wallet'
-                    value={walletId}
-                    label='Select Wallet'
-                    onChange={handleSelectWallet}>
-                    {wallets.map((wallet, index) => (
-                        <MenuItem
-                            className='DropdownItem'
-                            key={index}
-                            value={wallet.id}>
-                            <div className={'icon ' + wallet.icon} />
-                            <span>{wallet.name}</span>
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            {wallet && !walletInstalled
-                && <Alert severity="info" onClose={() => handleCleanSelections()}>Install {wallet.name} to connect to select a chain</Alert>}
-            {wallet && walletInstalled && <FormControl className='FormControl' fullWidth>
+  const handleSelectChain = (event: any) => {
+    const selectedChain = chains.find(
+      (chain) => chain.id === event.target.value
+    );
 
-                <InputLabel id='OriginChainDropdownLabel'>Origin Chain</InputLabel>
-                <Select
-                    id='OriginChainDropdown'
-                    labelId='OriginChainDropdown'
-                    value={chainId}
-                    label='Origin Chain'
-                    onChange={handleSelectChain}>
-                    {chains.map((chain, index) => (
-                        <MenuItem
-                            className='DropdownItem'
-                            key={index}
-                            value={chain.id}>
-                            <div className={'icon ' + chain.icon} />
-                            <span>{chain.name}</span>
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>}
-            {wallet && chain && isInstalled(wallet) && <div className='ConnectWalletFooter'>
-                <Button variant='contained'
-                    fullWidth
-                    onClick={() => handleConnectWallet()}>
-                    Use {wallet.name} with {chain?.name}
-                </Button>
-            </div>
-            }
+    if (wallet) {
+      setChainId(event.target.value);
+      setChain(selectedChain);
+
+      if (isConnected(wallet) && selectedChain) {
+        onWalletConnected(wallet, selectedChain);
+      }
+    }
+  };
+
+  const handleConnectWallet = async () => {
+    try {
+      if (wallet && chain) {
+        await connect(wallet, chain);
+
+        // For station and wallet connect
+        // the status must be checked on
+        // an imperative way thru useEffect
+        if (wallet.id !== "station" && wallet.id !== "stationmobile") {
+          onWalletConnected(wallet, chain);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      enqueueSnackbar("Operation cancelled", { variant: "error" });
+    }
+  };
+
+  const handleCleanSelections = () => {
+    setWallet(null);
+    setWalletId("");
+    setChain(null);
+    setChainId("");
+    setChains([]);
+    setWalletInstalled(false);
+  };
+
+  return (
+    <div className="ConnectWallet">
+      <h4>
+        Select a wallet and connect to an available network. Then, check your
+        wallet's airdrop eligibility, sign a transaction to prove ownership and
+        claim your airdrop.
+      </h4>
+      <FormControl className="FormControl" fullWidth>
+        <InputLabel id="WalletLabel">Select Wallet</InputLabel>
+        <Select
+          id="WalletDropdown"
+          labelId="Wallet"
+          value={walletId}
+          label="Select Wallet"
+          onChange={handleSelectWallet}
+        >
+          {wallets.map((wallet, index) => (
+            <MenuItem className="DropdownItem" key={index} value={wallet.id}>
+              <div className={"icon " + wallet.icon} />
+              <span>{wallet.name}</span>
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {wallet && !walletInstalled && (
+        <Alert severity="info" onClose={() => handleCleanSelections()}>
+          Install {wallet.name} to connect to select a chain
+        </Alert>
+      )}
+      {wallet && walletInstalled && (
+        <FormControl className="FormControl" fullWidth>
+          <InputLabel id="OriginChainDropdownLabel">Origin Chain</InputLabel>
+          <Select
+            id="OriginChainDropdown"
+            labelId="OriginChainDropdown"
+            value={chainId}
+            label="Origin Chain"
+            onChange={handleSelectChain}
+          >
+            {chains.map((chain, index) => (
+              <MenuItem className="DropdownItem" key={index} value={chain.id}>
+                <div className={"icon " + chain.icon} />
+                <span>{chain.name}</span>
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )}
+      {wallet && chain && isInstalled(wallet) && (
+        <div className="ConnectWalletFooter">
+          <Button
+            variant="contained"
+            fullWidth
+            onClick={() => handleConnectWallet()}
+          >
+            Use {wallet.name} with {chain?.name}
+          </Button>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
