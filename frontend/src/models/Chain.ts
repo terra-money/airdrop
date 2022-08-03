856fd0886ada30282cdec5607fbeda3dd9d54c5a
Emilian Harastasan <emilian@terra.money>
2022-08-03 15:28:00 +0200

    fix: chain id's

--- frontend/src/models/Chain.ts
+++ frontend/src/models/Chain.ts
@@ -1,5 +1,5 @@
-export type ChainId = 'terraclassic' | 'eth' | 'avax' | 'polygon' | 'sol' | 'axl' | 'injective' | 'juno' | 'kava' | 'fantom' | string;
-export type keplrChainId = 'columbus-5' | 'axelar-dojo-1' | 'injective-1' | 'juno-1' | 'kava_2222-10' | 'crypto-org-chain-mainnet-1';
+export type ChainId = 'terraclassic' | 'eth' | 'avax' | 'polygon' | 'sol' | 'axl' | 'injective' | 'juno' | 'kava' | 'fantom' | 'bsc' | string;
+export type keplrChainId = 'columbus-5' | 'axelar-dojo-1' | 'injective-1' | 'juno-1' | 'kava_2222-10';
 
 export type Chain = {
     id: ChainId,
