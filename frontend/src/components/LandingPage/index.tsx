292b79081ec4bba1878866cf42b9221de0bc7e0e
Emilian Harastasan <emilian@terra.money>
2022-08-03 17:33:18 +0200

    fix: linting on build

--- frontend/src/components/LandingPage/index.tsx
+++ frontend/src/components/LandingPage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Card, CardContent, CardActions, CardHeader } from '@mui/material';
+import { Button, Card, CardHeader } from '@mui/material';
 import './LandingPage.scss'
 
 
